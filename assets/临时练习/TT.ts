40647af7d0525a3ac9f16a0df6c8f6948e538a3c
Sky.Allen <894982165@qq.com>
2024-04-20 13:12:37 +0800

    2

--- assets/临时练习/TT.ts
+++ assets/临时练习/TT.ts
@@ -4,10 +4,8 @@ const { ccclass, property } = _decorator;
 @ccclass('TT')
 export class TT extends Component {
     start() {
-        let a=[3,2,1]
-        for(let [k,v] of a.entries()){
-            console.log(k+":"+v);
-        }
+
+
     }
 
     update(deltaTime: number) {
